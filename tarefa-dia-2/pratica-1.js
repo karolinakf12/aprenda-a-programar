39236941d15153817b06ac6a6840b61ccd9f4ad1
karla <karolinakf12@hotmail.com>
2022-08-28 15:01:29 -0300

    ola

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,2 +1,11 @@
 let nome = "Karla"
+let email = "karolinakf12@hotmail.com"
+let profissao = "Quimica"
+let idade = 32
+let viagemdossonhos = "Grecia"
+
 console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemdossonhos)
